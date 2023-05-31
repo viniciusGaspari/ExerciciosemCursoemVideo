a03c4248fa0c533a19827b423c8bbc2f4d3b0a51
unknown <vinicius7degaspari@gmail.com>
2023-05-31 19:06:09 -0300

    New project named Tabuada

---
+++ Projeto usando Condições e repetições!/Tabuada/script.js
@@ -0,0 +1,28 @@
+function create() {
+
+    let input = document.getElementById("number").value;
+
+    
+    if (input == '' ) {
+
+        alert("Insere número da tabuada")
+
+    }else {
+
+        let number = Number(input)
+        select.innerHTML = '';
+
+        for(let t = 1; t <= 10; t++){
+
+            const select = document.getElementById("select");
+            const option = document.createElement("option");
+
+            option.text = `${number} x ${t} = ${number*t}`
+            option.value = `n${t}`
+
+            select.appendChild(option);
+
+        }   
+        console.log(select)
+    }
+}
